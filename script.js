25616cedbd3f25a750b3617c27a9c0fd01c384df
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-09-09 00:53:56 +0900

    add anchor check

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var expire = new Date();
 expire.setDate(expire.getDate() + 1);
 console.log(expire.toUTCString());
 
-document.cookie = 'mycookie=this-is-my-cookie; \
-                   expire=' + expire.toUTCString();
+document.cookie = 'mycookie=this-is-my-cookie; expire=' + expire.toUTCString();
+
+var anchors = document.getElementsByTagName('a');
+for (var i = 0; i < anchors.length; ++i) {
+    href = anchors[i].getAttribute('href');
+    var domain = href.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
+    if (domain === document.domain) {
+        console.log(domain);
+    }
+}
+
 
